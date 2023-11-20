871404999e844befc8beb7371fb06a42cece8c3c
RenatoAtencio <renatoatencioaedo@gmail.com>
2023-11-19 22:54:47 -0300

    borre trabajo5 dentro de progEx

--- ProgramasExternos/Trabajo5/src/funcionesVerificacion.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/funciones.h"
-
-using namespace std;
-
-// Verifica la existencia de un archivo
-bool verificarArchivo(string path){
-    ifstream file(path);
-    return (file.good());
-}
